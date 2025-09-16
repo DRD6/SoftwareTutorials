088d489cc97ca161341ef6182a205d88db41fd50
Andrea De Vita <adevita@fcc-ironic-02.cern.ch>
2025-09-16 18:11:25 +0200

    ADD input file

--- GaudiTutorial/EventStats/options/runEventStatsSolution.py
+++ GaudiTutorial/EventStats/options/runEventStatsSolution.py
@@ -1,26 +1,29 @@
 from Gaudi.Configuration import INFO
 from k4FWCore import IOSvc, ApplicationMgr
-from Configurables import EventDataSvc
+from Configurables import EventDataSvc, AuditorSvc, ChronoAuditor
 
 io_svc = IOSvc("IOSvc")
-io_svc.Input = "/afs/cern.ch/work/a/adevita/public/DD4hepTutorials/simplecalo.root"
-io_svc.Output = "output.root"
+io_svc.Input = "../../data/simpleCalo_simulation.root"
+io_svc.Output = "../../data/simpleCalo_eventStats.root"
+
+chra = ChronoAuditor()
+audsvc = AuditorSvc()
+audsvc.Auditors = [chra]
 
 from Configurables import EventStats
 
 eventStats_functional = EventStats("EventStats",
-        
-    InputCaloHitCollection=["simplecaloRO"],
-    OutputEnergyBarycentre=["EnergyBarycentreX", "EnergyBarycentreY", "EnergyBarycentreZ"],
-    OutputTotalEnergy=["TotalEnergy"],
-    SaveHistograms=True,
-    OutputLevel=INFO
+    InputCaloHitCollection = ["simplecaloRO"],
+    OutputEnergyBarycentre = ["EnergyBarycentreX", "EnergyBarycentreY", "EnergyBarycentreZ"],
+    OutputTotalEnergy = ["TotalEnergy"],
+    SaveHistograms = True,
+    OutputLevel = INFO
 )
 
 app_mgr = ApplicationMgr(
-    TopAlg=[eventStats_functional],
-    EvtSel='NONE',
-    EvtMax=-1,
-    ExtSvc=[EventDataSvc("EventDataSvc")],
-    StopOnSignal=True,
+    TopAlg = [eventStats_functional],
+    EvtSel = 'NONE',
+    EvtMax = -1,
+    ExtSvc = [EventDataSvc("EventDataSvc"), audsvc],
+    StopOnSignal = True,
 )
